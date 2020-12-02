a55e1c0406a39434e54e34f3d2fc0d8a9c7b5c3d
Ty Carroll <typrograms.238@gmail.com>
2020-12-02 11:38:55 -0500

    final iteration

--- app/Main.hs
+++ app/Main.hs
@@ -4,16 +4,23 @@ import Graphics.Image as I
 
 main :: IO ()
 main = do
+  putStr "Input file name: "
+  imageFile <- getLine
+  -- let imageFile = "Olivia"
+  putStrLn "Operations: half, third, rotate90, flip, rotate180, vertFlip"
+  putStr "Choose your operation: "
   choose <- getLine
-  img <- I.readImageRGB VU "Olivia.jpg"
+  img <- I.readImageRGB VU (imageFile ++ ".jpg")
 
+  -- let imageFile = "Olivia.jpg"
+  
   case choose of 
-    "half" -> I.writeImage "Olivia_half.jpg" (halfImg img)
-    "third" -> I.writeImage "Olivia_third.jpg" (thirdImg img)
-    "rotate90" -> I.writeImage "Olivia_rotate.jpg" (rotate90 img)
-    "flip" -> I.writeImage "Olivia_flip.jpg" (flipIt img)
-    "rotate180" -> I.writeImage "Olivia_flip180.jpg" (rotate180 img)
-    "vertFlip" -> I.writeImage "Olivia_vertical.jpg" (flipV img)
+    "half" -> I.writeImage (imageFile ++ "_half.jpg") (halfImg img) 
+    "third" -> I.writeImage (imageFile ++ "_third.jpg") (thirdImg img)
+    "rotate90" -> I.writeImage (imageFile ++ "_rotate90.jpg") (rotate90 img)
+    "flip" -> I.writeImage (imageFile ++ "_flipIt.jpg") (flipIt img)
+    "rotate180" -> I.writeImage (imageFile ++ "_rotate180.jpg") (rotate180 img)
+    "vertFlip" -> I.writeImage (imageFile ++ "_vertFlip.jpg") (flipV img)
     _ -> putStrLn "Invalid option"
   
   
@@ -38,11 +45,32 @@ flipIt img = flipH img
 
 
 
+--1st time saved
+-- case choose of 
+--   "half" -> I.writeImage "Olivia_half.jpg" (halfImg img)
+--   "third" -> I.writeImage "Olivia_third.jpg" (thirdImg img)
+--   "rotate90" -> I.writeImage "Olivia_rotate.jpg" (rotate90 img)
+--   "flip" -> I.writeImage "Olivia_flip.jpg" (flipIt img)
+--   "rotate180" -> I.writeImage "Olivia_flip180.jpg" (rotate180 img)
+--   "vertFlip" -> I.writeImage "Olivia_vertical.jpg" (flipV img)
+--   _ -> putStrLn "Invalid option"
 
 
 
 
 
 
+--second time saving
+-- let imageFile = "Olivia"
+-- choose <- getLine
+-- img <- I.readImageRGB VU (imageFile ++ ".jpg")
 
 
+-- case choose of 
+--   "half" -> I.writeImage (imageFile ++ "_half.jpg") (halfImg img) 
+--   "third" -> I.writeImage imageFile (thirdImg img)
+--   "rotate90" -> I.writeImage imageFile . rotate90 $ img
+--   "flip" -> I.writeImage imageFile (flipIt img)
+--   "rotate180" -> I.writeImage imageFile (rotate180 img)
+--   "vertFlip" -> I.writeImage imageFile (flipV img)
+--   _ -> putStrLn "Invalid option"
